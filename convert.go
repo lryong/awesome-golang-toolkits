4f8784784563ac50244c595b26a7b9c2333e9f12
lryong <15816537946@163.com>
2018-10-09 23:44:44 +0800

    add convert.go

---
+++ convert.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func ConvertString(inter interface{}, precs ...int) string {
+	switch inter.(type) {
+	case string:
+		return v
+	case float64:
+		prec := 0
+		if len(precs) > 0 {
+			prec = precs[0]
+		}
+		return strconv.FormatFloat(v, 'f', prec, 64)
+	case int64:
+		return strconv.FormatInt(v, 10)
+	case uint64:
+		return strconv.FormatInt(v, 10)
+	case int:
+		return strconv.Itoa(v)
+	case uint:
+		return strconv.FormatUint(uint64(v), 10)
+	default:
+		return fmt.Sprintf("%v", inter)
+	}
+}
