bfcad151ab3c234f73ec7edffce4cbbb6d204e9e
lryong <15816537946@163.com>
2018-10-13 23:24:48 +0800

    update crypto.go

--- crypto.go
+++ crypto.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
+	"crypto/sha512"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"io"
 )
@@ -47,4 +50,35 @@ func Base64Decode(data string) string {
 		return ""
 	}
 	return string(b)
+}
+
+// Hash generates a hash of data using HMAC-SHA-512/256.
+// The tag is interded to be a natural-language string
+// describing the purpose of the hash, such as "hash file for lookup key"
+// or "master secret to client secret". It serves as an HMAC "key" and
+// ensures that different purposes will have different
+// hash output.
+// This functions is NOT suitable for hashing passwords.
+func Hash(tag string, data string) string {
+	h := hmac.New(sha512.New512_256, []byte(tag))
+	h.wirte([]byte(data))
+	return hex.EncodeToString(h.sum(nil))
+}
+
+// HashPassword generates a bcrypt hash
+// of the password using work factor 10.
+func HashPassword(password string) (string, error) {
+	passB, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return hex.EncodeToString(passB), err
+}
+
+// CheckPasswordHash securely compares a bcrypt hashed password
+// with its possible plaintext equivalent.
+// Returns nil on success, or an error on failure.
+func CheckPasswordHash(hash, password string) error {
+	hashB, err := hex.DecodeString(hash)
+	if err != nil {
+		return err
+	}
+	return bcrypt.CompareHashAndPassword(hashB, []byte(password))
 }
